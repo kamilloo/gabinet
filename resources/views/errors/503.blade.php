17d94fd546499b8a4076b67d662ef884e93807d3
kamilloo <kamil.pietka85@gmail.com>
2019-02-13 20:45:32 +0100

    maintain page view

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Strona w budowie</title>
+    <style>
+        * {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        body {
+            color: #2b2b2b;
+            font-size: 16px;
+            line-height: normal;
+        }
+
+        .page {
+            width: 100%;
+            height: 100vh;
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            align-items: center;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            justify-content: center;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .content h1 {
+            margin: 0px;
+        }
+        .content h2 {
+            margin: 0px;
+        }
+
+        .content p {
+            margin-bottom: 0px;
+            margin-top: 25px;
+        }
+    </style>
+</head>
+<body>
+<main class="page">
+    <section class="content">
+        <h1>Strona w budowie</h1>
+        <br>
+        <h5>Gabinet Kosmetyki Pielęgnacyjnej Katarzyna Piętka</h5>
+        <p>Zmieniamy się dla Ciebie</p>
+        <p>tel.: +48 602 139 040</p>
+    </section>
+</main>
+</body>
+</html>
